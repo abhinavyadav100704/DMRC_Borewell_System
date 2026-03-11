e950c8386f5be65461840166c83a019ab6850f3f
abhinavyadav100704 <abhinavyadav100704@gmail.com>
2026-03-12 00:29:40 +0530

    small changes in test file

--- BACKEND/src/test/java/com/dmrc/borewell/BorewellSystemApplicationTests.java
+++ BACKEND/src/test/java/com/dmrc/borewell/BorewellSystemApplicationTests.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJson;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureJson
 @AutoConfigureMockMvc
 class BorewellSystemApplicationTests {
 
